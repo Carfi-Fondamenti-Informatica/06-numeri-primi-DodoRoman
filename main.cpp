a948ed4d1e1ecea074e5be615411cc87854fb8af
DodoRoman <115349003+DodoRoman@users.noreply.github.com>
2022-12-01 11:37:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#iclude "lib.h"
+using namespace std;
 
 int main(){
+    int n=0;
+    cin >> n;
+    if (primoo(n)==1){
+        cout << "numero primo" << endl;
+    }
+    else {
+        cout << "numero non primo" << endl;
+    }
   return 0;
 }
