fa831819c8d20a8e89dc0453a650de7b1737ab30
DodoRoman <115349003+DodoRoman@users.noreply.github.com>
2022-12-01 11:35:49 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
 
+bool primoo(int n){
+    if (n>1){
+        for (int i=2; i<n; i++){
+            if (n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
